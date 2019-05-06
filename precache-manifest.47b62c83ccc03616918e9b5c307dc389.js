f4a079afb19001fb47084716b6604550ffcb2385
Ki <kikim1025@gmail.com>
2019-05-06 15:40:51 -0400

    Updates

---
+++ precache-manifest.47b62c83ccc03616918e9b5c307dc389.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67064de24b1457b617dd970e39b39b15",
+    "url": "/Reacting-Calculadora/index.html"
+  },
+  {
+    "revision": "c29f10592cce6459425b",
+    "url": "/Reacting-Calculadora/static/css/main.a9ec98ea.chunk.css"
+  },
+  {
+    "revision": "7d120be810a1c6f9203c",
+    "url": "/Reacting-Calculadora/static/js/2.ac4ab322.chunk.js"
+  },
+  {
+    "revision": "c29f10592cce6459425b",
+    "url": "/Reacting-Calculadora/static/js/main.3df23458.chunk.js"
+  },
+  {
+    "revision": "c35f977979905e5f8ca4",
+    "url": "/Reacting-Calculadora/static/js/runtime~main.de5dd775.js"
+  }
+]);
